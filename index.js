85a3920cad86f307ff95a52f449ab7dd67937248
Atanas Atanasov <scriptex.bg@gmail.com>
2021-04-14 15:47:23 +0300

    Switch to SVGO v2

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
 #!/usr/bin/env node
 
-// @ts-nocheck
-
 const { resolve } = require('path');
 const { readFileSync } = require('fs');
 const { readdir, readFile, writeFile } = require('fs').promises;
 
-const SVGO = require('svgo');
-const yaml = require('js-yaml');
-
-let counter = 0;
-
-const getAttr = (svg, attr) => parseFloat(svg.attr(attr).value.replace(/px$/, ''));
-
-const addViewBox = svg => {
-	if (svg.isElem(['svg']) && !svg.hasAttr('viewBox') && svg.hasAttr('width') && svg.hasAttr('height')) {
-		const width = getAttr(svg, 'width');
-		const height = getAttr(svg, 'height');
-
-		svg.addAttr({
-			name: 'viewBox',
-			local: 'viewBox',
-			value: '0 0 ' + width + ' ' + height,
-			prefix: ''
-		});
-	}
-
-	return svg;
-};
+const { optimize, loadConfig } = require('svgo');
 
 async function getFiles(dir) {
 	const dirents = await readdir(dir, { withFileTypes: true });
@@ -44,6 +21,9 @@ async function getFiles(dir) {
 module.exports = async args => {
 	const { input, svgoFile } = args;
 
+	/**
+	 * @type any
+	 */
 	let svgoConfig;
 
 	try {
@@ -51,35 +31,8 @@ module.exports = async args => {
 	} catch (e) {
 		console.warn('Invalid or missing SVGO config file! Using default.');
 
-		svgoConfig = readFileSync(resolve(__dirname, 'svgo.yml'));
+		svgoConfig = await loadConfig(resolve(__dirname, 'svgo.config.js'));
 	}
-
-	const { plugins } = yaml.safeLoad(svgoConfig);
-
-	const svgo = new SVGO({
-		plugins: [
-			...plugins.map(plugin => ({
-				[plugin]: true
-			})),
-			{
-				cleanupIDs: {
-					prefix: {
-						toString: () => `id-${counter++}`
-					}
-				}
-			},
-			{
-				custom: {
-					type: 'perItem',
-					active: true,
-					// prettier-ignore
-					description: 'Plugin which adds viewBox based on width and height',
-					params: {},
-					fn: addViewBox
-				}
-			}
-		]
-	});
 
 	const folder = resolve(input);
 	const files = await getFiles(folder);
@@ -87,8 +40,8 @@ module.exports = async args => {
 
 	for (const path of paths) {
 		const data = await readFile(path, 'utf-8');
-		const result = await svgo.optimize(data, { path });
+		const result = optimize(data, svgoConfig);
 
-		await writeFile(result.path, result.data);
+		await writeFile(path, result.data);
 	}
 };
