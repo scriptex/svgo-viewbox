3c650bcbc3f5c45ee2d0a394f35ff3d025350464
Atanas Atanasov <scriptex.bg@gmail.com>
2021-07-27 14:14:22 +0300

    Fix tests

--- test.js
+++ test.js
@@ -7,25 +7,26 @@ const tape = require('tape');
 const { parse } = require('svg-parser');
 
 const svgoViewBox = require('.');
-
 const file = './assets/logo.svg';
 
-svgoViewBox({
-	input: './assets'
-}).then(() => {
-	tape('file exists', t => {
-		const fileExists = existsSync(resolve(__dirname, file));
-
-		t.ok(fileExists, 'File exists');
-		t.end();
+(async () => {
+	await svgoViewBox({
+		input: './assets'
 	});
+})();
 
-	tape('viewBox attribute is added', t => {
-		const svg = readFileSync(file, 'utf-8');
-		const parsed = parse(svg);
-		const hasViewBox = !!parsed.children[0].properties.viewBox;
+tape('file exists', t => {
+	const fileExists = existsSync(resolve(__dirname, file));
 
-		t.ok(hasViewBox, 'Has viewBox attribute');
-		t.end();
-	});
+	t.ok(fileExists, 'File exists');
+	t.end();
+});
+
+tape('viewBox attribute is added', t => {
+	const svg = readFileSync(file, 'utf-8');
+	const parsed = parse(svg);
+	const hasViewBox = !!parsed.children[0].properties.viewBox;
+
+	t.ok(hasViewBox, 'Has viewBox attribute');
+	t.end();
 });
