8ca62c3d2168418f0b054fab623f875b3b7b7d72
Atanas Atanasov <hi@atanas.info>
2023-08-08 13:43:45 +0300

    Improve tests

--- test.js
+++ test.js
@@ -1,12 +1,44 @@
 const { resolve } = require('path');
 const { execSync } = require('child_process');
-const { existsSync, readFileSync } = require('fs');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 
 const tape = require('tape');
 const { parse } = require('svg-parser');
 
 const svgoViewBox = require('.');
 const { getFiles } = require('./utils');
+
+const assets = ['assets', 'assets2']
+	.map(folder => resolve(__dirname, folder, 'logo.svg'))
+	.map(file => ({
+		[file]: readFileSync(file, 'utf-8')
+	}));
+
+function resetFilesContents() {
+	for (const asset of assets) {
+		const [file, content] = Object.entries(asset)[0];
+		writeFileSync(file, content);
+	}
+}
+
+async function executeTest(input, t) {
+	const files = await getFiles(resolve(input || process.cwd()));
+
+	for (const file of files) {
+		const svg = readFileSync(file, 'utf-8');
+		const parsed = parse(svg);
+		// @ts-ignore
+		const hasViewBox = !!parsed.children[0].properties.viewBox;
+		const fileExists = existsSync(resolve(__dirname, file));
+
+		t.ok(fileExists, 'File exists');
+		t.ok(hasViewBox, 'Has viewBox attribute');
+	}
+
+	resetFilesContents();
+
+	t.end();
+}
 
 function addViewbox(message, input, paths = []) {
 	tape(message, async t => {
@@ -17,28 +49,15 @@ function addViewbox(message, input, paths = []) {
 			paths
 		);
 
-		const files = await getFiles(resolve(input || process.cwd()));
-
-		for (const file of files) {
-			const svg = readFileSync(file, 'utf-8');
-			const parsed = parse(svg);
-			// @ts-ignore
-			const hasViewBox = !!parsed.children[0].properties.viewBox;
-			const fileExists = existsSync(resolve(__dirname, file));
-
-			t.ok(fileExists, 'File exists');
-			t.ok(hasViewBox, 'Has viewBox attribute');
-		}
-
-		t.end();
+		await executeTest(input, t);
 	});
 }
 
-function addViewboxCLI(input, args = '') {
-	tape('Executes CLI commands', t => {
+function addViewboxCLI(message, input, args = '') {
+	tape('Executes CLI commands', async t => {
 		execSync(`./cli.js -i ${input} ${args}`);
 
-		t.end();
+		await executeTest(input, t);
 	});
 }
 
@@ -50,5 +69,5 @@ addViewbox('Adds viewBox to files in a folder', undefined, ['./assets', './asset
 addViewbox('Adds viewBox to files in a folder', './assets', ['./assets/logo.svg', './assets2']);
 addViewbox('Adds viewBox to a single file', './assets2/logo.svg', ['./assets', './assets2/logo.svg']);
 
-addViewboxCLI('./assets', '-f ./svgo.config.js');
-addViewboxCLI('./assets2');
+addViewboxCLI('Uses an external SVGO config', './assets', '-f ./svgo.config.js');
+addViewboxCLI('Uses the built-in SVGO config', './assets2');
